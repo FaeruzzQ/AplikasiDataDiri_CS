d784d59b8e87bb787281d49f4f859f0ac98b8c58
FaeruzzQ <128023607+FaeruzzQ@users.noreply.github.com>
2023-09-21 10:22:15 +0700

    Add files via upload

---
+++ TugasTiga1/TugasTiga1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TugasTiga1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "NIM : 22.11.5155";
+            label2.Text = "Nama : Qubussima Faeroz Furqona";
+            label3.Text = "Kelas : 22-IF10";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "NIM :";
+            label2.Text = "Nama :";
+            label3.Text = "Kelas :";
+        }
+    }
+}
